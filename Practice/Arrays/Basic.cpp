45f88e4b992002554c7b9df97a0fcaed0cd98641
Pawan bisht <pawanbisht83@gmail.com>
2020-05-29 06:00:00 +0530

    This the array and correct command

---
+++ Practice/Arrays/Basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10] = {1, 2, 3}; //Initialize first Three element as 1,2,3 then zero after that
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}

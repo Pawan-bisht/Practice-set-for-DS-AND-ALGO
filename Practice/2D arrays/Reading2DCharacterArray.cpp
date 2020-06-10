2775cc6e632feee5e34624e728a64f4c69edf4c8
Pawan bisht <pawanbisht83@gmail.com>
2020-06-10 18:41:37 +0530

    This is the commit for reading 2D character array

---
+++ Practice/2D arrays/Reading2DCharacterArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char arr[100][100];
+    int n;
+    cin >> n;
+    cin.get();
+    for (int i = 0; i < n; i++)
+    {
+        cin.getline(arr[i], 1000);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
